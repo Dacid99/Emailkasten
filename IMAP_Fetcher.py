6a386b369bc68981a7ea9b68730edae50599f5a6
SBV <18516197-Dacid99@users.noreply.gitlab.com>
2024-07-31 20:51:55 +0200

    added fetcher for imap

---
+++ IMAP_Fetcher.py
@@ -0,0 +1,23 @@
+import imaplib
+import os
+
+from MailParser import MailParser
+
+class IMAP_Fetcher(imaplib.IMAP4): 
+
+    def __init__(self, username, password, host: str = "", port: int = 993, timeout= None):
+        super().__init__(host, port, timeout)
+        self.__username = username
+        self.__password = password
+
+    
+    def fetchAndPrintAll(self, mailbox = 'INBOX'):
+        self.login(self.__username, self.__password)
+        self.select(mailbox)
+        typ, data = self.search(None, 'ALL')
+        for number in data[0].split()[-2:]:
+            typ, data = self.fetch(number, '(RFC822)')
+            print('Message %s\n%s\n' % (number, MailParser.parseTo(data[0][1]))) 
+        self.logout()
+
+
