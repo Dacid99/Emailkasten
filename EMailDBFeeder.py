98b1c5594fa66bed2209c4e7c26830b5f2acda3c
SBV <18516197-Dacid99@users.noreply.gitlab.com>
2024-08-03 20:38:07 +0200

    adapted to dbmanager introduction

--- EMailDBFeeder.py
+++ EMailDBFeeder.py
@@ -29,102 +29,70 @@ class EMailDBFeeder:
     MENTION_BCC = "BCC"
 
 
-    def __init__(self, host, user, password, database, charset, collation):
-        try:
-            self.__dbConnection = mysql.connector.connect(
-                host=host,
-                user=user,
-                password=password,
-                database=database,
-                charset=charset,
-                collation=collation
-            )
-            self.__dbCursor = self.__dbConnection.cursor()
-            self.__isHealthy = True
-
-        except mysql.connector.Error as e:
-            traceback.print_exc()
-            self.__isHealthy = False
+    def __init__(self, dbManager):
+        self.__dbManager = dbManager
 
     def insertEmail(self, parsedEMail):
-        if self.__isHealthy:
-            emailData = []
-            emailData.append(parsedEMail.parseMessageID())
-            emailData.append(parsedEMail.parseFrom()[1])
-            emailData.append(parsedEMail.parseDate())
-            emailData.append(parsedEMail.parseBody())
-            try:
-                self.__dbCursor.execute(EMailDBFeeder.__INSERT_EMAIL_SQL, emailData)
-                return self.__dbCursor.lastrowid
-            except Exception as e:
-                traceback.print_exc()
-                self.__isHealthy = False
+        emailData = []
+        emailData.append(parsedEMail.parseMessageID())
+        emailData.append(parsedEMail.parseFrom()[1])
+        emailData.append(parsedEMail.parseDate())
+        emailData.append(parsedEMail.parseBody())
+        return self.__dbManager.execute(EMailDBFeeder.__INSERT_EMAIL_SQL, emailData)
 
     def insertCorrespondents(self, parsedEMail):
-        if self.__isHealthy:
 
-            toCorrespondents = parsedEMail.parseTo()
-            ccCorrespondents = parsedEMail.parseCc()
-            bccCorrespondents = parsedEMail.parseBcc()
+        toCorrespondents = parsedEMail.parseTo()
+        ccCorrespondents = parsedEMail.parseCc()
+        bccCorrespondents = parsedEMail.parseBcc()
 
-            try:
+        for toCorrespondent in toCorrespondents:
+            self.__dbManager.execute(EMailDBFeeder.__INSERT_CORRESPONDENT_SQL, toCorrespondent)
+        for ccCorrespondent in ccCorrespondents:
+            self.__dbManager.execute(EMailDBFeeder.__INSERT_CORRESPONDENT_SQL, ccCorrespondent)
+        for bccCorrespondent in bccCorrespondents:
+            self.__dbManager.execute(EMailDBFeeder.__INSERT_CORRESPONDENT_SQL, bccCorrespondent)
 
-                for toCorrespondent in toCorrespondents:
-                    self.__dbCursor.execute(EMailDBFeeder.__INSERT_CORRESPONDENT_SQL, toCorrespondent)
-                for ccCorrespondent in ccCorrespondents:
-                    self.__dbCursor.execute(EMailDBFeeder.__INSERT_CORRESPONDENT_SQL, ccCorrespondent)
-                for bccCorrespondent in bccCorrespondents:
-                    self.__dbCursor.execute(EMailDBFeeder.__INSERT_CORRESPONDENT_SQL, bccCorrespondent)
-
-            except Exception as e:
-                traceback.print_exc()
-                self.__isHealthy = False
 
     def insertEmailCorrespondentsConnection(self, parsedEMail, emailID):
-        if self.__isHealthy:
 
-            toCorrespondentsAddresses = []
-            for toCorrespondent in parsedEMail.parseTo():
-                toCorrespondentsAddresses.append(toCorrespondent[1])
-            ccCorrespondentsAddresses = []
-            for ccCorrespondent in parsedEMail.parseCc():
-                ccCorrespondentsAddresses.append(ccCorrespondent[1])
-            bccCorrespondentsAddresses = []
-            for bccCorrespondent in parsedEMail.parseBcc():
-                bccCorrespondentsAddresses.append(bccCorrespondent[1])
+        toCorrespondentsAddresses = []
+        for toCorrespondent in parsedEMail.parseTo():
+            toCorrespondentsAddresses.append(toCorrespondent[1])
 
-            try: 
-                self.__dbCursor.execute(EMailDBFeeder.__SELECT_CORRESPONDENTS_ID_SQL % ', '.join(['%s']*len(toCorrespondentsAddresses)), toCorrespondentsAddresses)
-                toCorrespondentsIDs = self.__dbCursor.fetchall()
-                
-                self.__dbCursor.execute(EMailDBFeeder.__SELECT_CORRESPONDENTS_ID_SQL % ', '.join(['%s']*len(ccCorrespondentsAddresses)), ccCorrespondentsAddresses)
-                ccCorrespondentsIDs = self.__dbCursor.fetchall()
-                
-                self.__dbCursor.execute(EMailDBFeeder.__SELECT_CORRESPONDENTS_ID_SQL % ', '.join(['%s']*len(bccCorrespondentsAddresses)), bccCorrespondentsAddresses)
-                bccCorrespondentsIDs = self.__dbCursor.fetchall()
-                
-                for toCorrespondentId in toCorrespondentsIDs:
-                    self.__dbCursor.execute(EMailDBFeeder.__INSERT_EMAIL_CORRESPONDENT_CONNECTION_SQL, [emailID, toCorrespondentId[0], EMailDBFeeder.MENTION_TO])
-                for ccCorrespondentId in ccCorrespondentsIDs:
-                    self.__dbCursor.execute(EMailDBFeeder.__INSERT_EMAIL_CORRESPONDENT_CONNECTION_SQL, (emailID, ccCorrespondentId[0], EMailDBFeeder.MENTION_CC))
-                for bccCorrespondentId in bccCorrespondentsIDs:
-                    self.__dbCursor.execute(EMailDBFeeder.__INSERT_EMAIL_CORRESPONDENT_CONNECTION_SQL, (emailID, bccCorrespondentId[0], EMailDBFeeder.MENTION_BCC))
+        ccCorrespondentsAddresses = []
+        for ccCorrespondent in parsedEMail.parseCc():
+            ccCorrespondentsAddresses.append(ccCorrespondent[1])
 
-            except Exception as e:
-                traceback.print_exc()
-                self.__isHealthy = False
+        bccCorrespondentsAddresses = []
+        for bccCorrespondent in parsedEMail.parseBcc():
+            bccCorrespondentsAddresses.append(bccCorrespondent[1])
 
-    
+
+        self.__dbManager.execute(EMailDBFeeder.__SELECT_CORRESPONDENTS_ID_SQL % ', '.join(['%s']*len(toCorrespondentsAddresses)), toCorrespondentsAddresses)
+        toCorrespondentsIDs = self.__dbManager.fetchall()
+        
+        self.__dbManager.execute(EMailDBFeeder.__SELECT_CORRESPONDENTS_ID_SQL % ', '.join(['%s']*len(ccCorrespondentsAddresses)), ccCorrespondentsAddresses)
+        ccCorrespondentsIDs = self.__dbManager.fetchall()
+        
+        self.__dbManager.execute(EMailDBFeeder.__SELECT_CORRESPONDENTS_ID_SQL % ', '.join(['%s']*len(bccCorrespondentsAddresses)), bccCorrespondentsAddresses)
+        bccCorrespondentsIDs = self.__dbManager.fetchall()
+
+        
+        for toCorrespondentId in toCorrespondentsIDs:
+            self.__dbManager.execute(EMailDBFeeder.__INSERT_EMAIL_CORRESPONDENT_CONNECTION_SQL, [emailID, toCorrespondentId[0], EMailDBFeeder.MENTION_TO])
+
+        for ccCorrespondentId in ccCorrespondentsIDs:
+            self.__dbManager.execute(EMailDBFeeder.__INSERT_EMAIL_CORRESPONDENT_CONNECTION_SQL, (emailID, ccCorrespondentId[0], EMailDBFeeder.MENTION_CC))
+
+        for bccCorrespondentId in bccCorrespondentsIDs:
+            self.__dbManager.execute(EMailDBFeeder.__INSERT_EMAIL_CORRESPONDENT_CONNECTION_SQL, (emailID, bccCorrespondentId[0], EMailDBFeeder.MENTION_BCC))
+
+
     def insert(self, parsedEMail):
         emailID = self.insertEmail(parsedEMail)
         self.insertCorrespondents(parsedEMail)
         self.insertEmailCorrespondentsConnection(parsedEMail, emailID)
-        if self.__isHealthy:
-            self.__dbConnection.commit()
+        self.__dbManager.commit()
 
-
-    def __del__(self):
-        if self.__dbConnection.is_connected():
-            self.__dbCursor.close()
-            self.__dbConnection.close()
         
