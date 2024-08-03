cdf8f2a57636efa879b137a74b8a7e6b98569ffe
SBV <18516197-Dacid99@users.noreply.gitlab.com>
2024-08-03 22:16:14 +0200

    updated test for db

--- DBTest.py
+++ DBTest.py
@@ -2,14 +2,15 @@ from EMailDBFeeder import EMailDBFeeder
 from IMAP_SSL_Fetcher import IMAP_SSL_Fetcher
 from POP3_SSL_Fetcher import POP3_SSL_Fetcher
 from MailParser import MailParser
-
+from DBManager import DBManager
 
 
 imapMail = IMAP_SSL_Fetcher(username="archiv@aderbauer.org", password="nxF154j9879ZZsW", host="imap.ionos.de", port=993)
 popMail = POP3_SSL_Fetcher(username="archiv@aderbauer.org", password="nxF154j9879ZZsW", host="pop.ionos.de")
 
-parsedLatestMail = popMail.fetchLatest()
+parsedLatestMail = imapMail.fetchLatest()
 
-db = EMailDBFeeder("192.168.178.109", "root", "example", "email_archive", "utf8mb4", "utf8mb4_bin")
+with DBManager("192.168.178.109", "root", "example", "email_archive", "utf8mb4", "utf8mb4_bin") as db:
+    dbfeeder = EMailDBFeeder(db)
 
-db.insert(parsedLatestMail)
+    dbfeeder.insert(parsedLatestMail)
