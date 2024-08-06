77f7b83dce57fbfbadfefe5eb32243245b43ef3f
SBV <18516197-Dacid99@users.noreply.gitlab.com>
2024-08-06 18:42:25 +0200

    formatted logging and added console option

--- LoggerFactory.py
+++ LoggerFactory.py
@@ -5,8 +5,10 @@ class LoggerFactory:
     loggerName = "EMailArchiverDaemon"
     logfilePath = f"/var/log/{loggerName}.log"
     logLevel = logging.INFO
-    logfileMaxSize = 10 * 1024 * 1024 # 1 MB
+    logfileMaxSize = 10 * 1024 * 1024 # 10 MB
     logfileBackupCount = 3 
+    consoleLogging = False
+    logFormat = '%(name)s - %(levelname)s: %(message)s'
 
     @staticmethod
     def getMainLogger():
@@ -15,7 +17,17 @@ class LoggerFactory:
 
         logfileHandler= logging.handlers.RotatingFileHandler(LoggerFactory.logfilePath, maxBytes = LoggerFactory.logfileMaxSize, backupCount = LoggerFactory.logfileBackupCount)
         logfileHandler.setLevel(LoggerFactory.logLevel)
+        
+        formatter = logging.Formatter(LoggerFactory.logFormat)
+        logfileHandler.setFormatter(formatter)
+        
         logger.addHandler(logfileHandler)
+
+        if LoggerFactory.consoleLogging:
+            consoleHandler = logging.StreamHandler()
+            consoleHandler.setLevel(LoggerFactory.logLevel)
+            consoleHandler.setFormatter(formatter)
+            logger.addHandler(consoleHandler)
 
         return logger
 
