5d59b25fe09504d66594685e40a091a47b3f52bc
SBV <18516197-Dacid99@users.noreply.gitlab.com>
2024-08-30 17:32:48 +0200

    added case sensitive filter, improved queryparam in correspondents

--- Emailkasten/Filters/CorrespondentFilter.py
+++ Emailkasten/Filters/CorrespondentFilter.py
@@ -2,10 +2,11 @@ import django_filters
 from ..Models.CorrespondentModel import CorrespondentModel
 
 class CorrespondentFilter(django_filters.FilterSet):
+    mention__exact = django_filters.CharFilter(field_name='correspondentemails__mention', lookup_expr='exact')
+
     class Meta:
         model = CorrespondentModel
         fields = {
-            'email_name': ['icontains', 'exact'],
-            'email_address': ['icontains', 'exact'],
-            'correspondentemails__mention': ['exact']
+            'email_name': ['icontains', 'contains', 'exact'],
+            'email_address': ['icontains', 'contains', 'exact']
         }
--- Emailkasten/Filters/EMailFilter.py
+++ Emailkasten/Filters/EMailFilter.py
@@ -3,23 +3,28 @@ from ..Models.EMailModel import EMailModel
 
 class EMailFilter(django_filters.FilterSet):
     attachment_name__icontains = django_filters.CharFilter(field_name='attachments__file_name', lookup_expr='icontains')
+    attachment_name__contains = django_filters.CharFilter(field_name='attachments__file_name', lookup_expr='contains')
     attachment_name__exact = django_filters.CharFilter(field_name='attachments__file_name', lookup_expr='exact')
     correspondent_name__icontains = django_filters.CharFilter(field_name="emailcorrespondents__correspondent__email_name", lookup_expr='icontains')
+    correspondent_name__contains = django_filters.CharFilter(field_name="emailcorrespondents__correspondent__email_name", lookup_expr='contains')
     correspondent_name__exact = django_filters.CharFilter(field_name="emailcorrespondents__correspondent__email_name", lookup_expr='exact')
     correspondent_address__icontains = django_filters.CharFilter(field_name="emailcorrespondents__correspondent__email_address", lookup_expr='icontains')
+    correspondent_address__contains = django_filters.CharFilter(field_name="emailcorrespondents__correspondent__email_address", lookup_expr='contains')
     correspondent_address__exact = django_filters.CharFilter(field_name="emailcorrespondents__correspondent__email_address", lookup_expr='exact')
     correspondent_mention__exact = django_filters.CharFilter(field_name='emailcorrespondents__mention', lookup_expr='exact')
     account_address__icontains = django_filters.CharFilter(field_name='in_account__address', lookup_expr="icontains")
+    account_address__contains = django_filters.CharFilter(field_name='in_account__address', lookup_expr="contains")
     account_address__exact = django_filters.CharFilter(field_name='in_account__address', lookup_expr="exact")
     account_host__icontains = django_filters.CharFilter(field_name='in_account__mail_host', lookup_expr="icontains")
+    account_host__contains = django_filters.CharFilter(field_name='in_account__mail_host', lookup_expr="contains")
     account_host__exact = django_filters.CharFilter(field_name='in_account__mail_host', lookup_expr="exact")
 
     class Meta:
         model = EMailModel
         fields = {
-            'message_id': ['icontains', 'exact'],
+            'message_id': ['icontains', 'contains', 'exact'],
             'datetime': ['gte', 'lte'],
-            'email_subject': ['icontains', 'exact'],
-            'bodytext': ['icontains'],
+            'email_subject': ['icontains', 'contains', 'exact'],
+            'bodytext': ['icontains', 'contains'],
             'datasize': ['gte', 'lte']
         }
