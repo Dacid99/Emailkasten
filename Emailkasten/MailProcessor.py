ecc9f703a523e0e32a8e78b79feb3637cc503bd4
david <18516197-Dacid99@users.noreply.gitlab.com>
2024-10-03 20:09:16 +0200

    added first draft of prerendering of eml

--- Emailkasten/MailProcessor.py
+++ Emailkasten/MailProcessor.py
@@ -8,6 +8,14 @@ from .LoggerFactory import LoggerFactory
 from .MailParser import MailParser
 from .EMailDBFeeder import EMailDBFeeder
 import datetime
+import email
+import email.header
+import imgkit
+import os
+import quopri
+import base64
+import hashlib
+from PIL import Image
 from . import constants
 
 class MailProcessor:
@@ -150,3 +158,156 @@ class MailProcessor:
         for parsedMail in parsedMailsList:
             EMailDBFeeder.insertEMail(parsedMail, mailAccount)
         logger.debug("Successfully wrote emails to database")
+
+
+    @staticmethod
+    def processEml(data):
+        # Create the dump directory if not existing yet
+        if not os.path.isdir(dumpDir):
+            os.makedirs(dumpDir)
+            writeLog("[INFO] Created dump directory %s" % dumpDir)
+
+        msg = email.message_from_bytes(data)
+        try:
+            decode = email.header.decode_header(msg['Date'])[0]
+            dateField = str(decode[0])
+        except:
+            dateField = '&lt;Unknown&gt;'
+        writeLog('[INFO] Date: %s' % dateField)
+
+        try:
+            decode = email.header.decode_header(msg['From'])[0]
+            fromField = str(decode[0])
+        except:
+            fromField = '&lt;Unknown&gt;'
+        writeLog('[INFO] From: %s' %  fromField)
+        fromField = fromField.replace('<', '&lt;').replace('>', '&gt;')
+
+        try:
+            decode = email.header.decode_header(msg['To'])[0]
+            toField = str(decode[0])
+        except:
+            toField = '&lt;Unknown&gt;'
+        writeLog('[INFO] To: %s' % toField)
+        toField = toField.replace('<', '&lt;').replace('>', '&gt;')
+
+        try:
+            decode = email.header.decode_header(msg['Subject'])[0]
+            subjectField = str(decode[0])
+        except:
+            subjectField = '&lt;Unknown&gt;'
+        writeLog('[INFO] Subject: %s' % subjectField)
+        subjectField = subjectField.replace('<', '&lt;').replace('>', '&gt;')
+
+        try:
+            decode = email.header.decode_header(msg['Message-Id'])[0]
+            idField = str(decode[0])
+        except:
+            idField = '&lt;Unknown&gt;'
+        writeLog('[INFO] Message-Id: %s' % idField)
+        idField = idField.replace('<', '&lt;').replace('>', '&gt;')    
+
+        imgkitOptions = { 'load-error-handling': 'skip'}
+        # imgkitOptions.update({ 'quiet': None })
+        imagesList = []
+        attachments = []
+
+        # Build a first image with basic mail details
+        headers = '''
+        <table width="100%%">
+        <tr><td align="right"><b>Date:</b></td><td>%s</td></tr>
+        <tr><td align="right"><b>From:</b></td><td>%s</td></tr>
+        <tr><td align="right"><b>To:</b></td><td>%s</td></tr>
+        <tr><td align="right"><b>Subject:</b></td><td>%s</td></tr>
+        <tr><td align="right"><b>Message-Id:</b></td><td>%s</td></tr>
+        </table>
+        <hr></p>
+        ''' % (dateField, fromField, toField, subjectField, idField)
+        m = hashlib.md5()
+        m.update(headers.encode('utf-8'))
+        imagePath = m.hexdigest() + '.png'
+        try:
+            imgkit.from_string(headers, dumpDir + '/' + imagePath, options = imgkitOptions)
+            writeLog('[INFO] Created headers %s' % imagePath)
+            imagesList.append(dumpDir + '/' + imagePath)
+        except:
+            writeLog('[WARNING] Creation of headers failed')
+
+        #
+        # Main loop - process the MIME parts
+        #
+        for part in msg.walk():
+            mimeType = part.get_content_type()
+            if part.is_multipart():
+                writeLog('[INFO] Multipart found, continue')
+                continue
+
+            writeLog('[INFO] Found MIME part: %s' % mimeType)
+            if mimeType in textTypes:
+                try:
+                    payload = quopri.decodestring(part.get_payload(decode=True)).decode('utf-8')
+                except:
+                    payload = str(quopri.decodestring(part.get_payload(decode=True)))[2:-1]
+                
+                # Cleanup dirty characters
+                dirtyChars = [ '\n', '\\n', '\t', '\\t', '\r', '\\r']
+                for char in dirtyChars:
+                    payload = payload.replace(char, '')
+                
+                # Generate MD5 hash of the payload
+                m = hashlib.md5()
+                m.update(payload.encode('utf-8'))
+                imagePath = m.hexdigest() + '.png'
+                try:
+                    imgkit.from_string(payload, dumpDir + '/' + imagePath, options = imgkitOptions)
+                    writeLog('[INFO] Decoded %s' % imagePath)
+                    imagesList.append(dumpDir + '/' + imagePath)
+                except:
+                    writeLog('[WARNING] Decoding this MIME part returned error')
+            elif mimeType in imageTypes:
+                payload = part.get_payload(decode=False)
+                imgdata = base64.b64decode(payload)
+                # Generate MD5 hash of the payload
+                m = hashlib.md5()
+                m.update(payload.encode('utf-8'))
+                imagePath = m.hexdigest() + '.' + mimeType.split('/')[1]
+                try:
+                    with open(dumpDir + '/' + imagePath, 'wb') as f:
+                        f.write(imgdata)
+                    writeLog('[INFO] Decoded %s' % imagePath)
+                    imagesList.append(dumpDir + '/' + imagePath)
+                except:
+                    writeLog('[WARNING] Decoding this MIME part returned error')
+            else:
+                fileName = part.get_filename()
+                if not fileName:
+                    fileName = "Unknown"
+                attachments.append("%s (%s)" % (fileName, mimeType))
+                writeLog('[INFO] Skipped attachment %s (%s)' % (fileName, mimeType))
+
+        if len(attachments):
+            footer = '<p><hr><p><b>Attached Files:</b><p><ul>'
+            for a in attachments:
+                footer = footer + '<li>' + a + '</li>'
+            footer = footer + '</ul><p><br>Generated by EMLRender v1.0'
+            m = hashlib.md5()
+            m.update(footer.encode('utf-8'))
+            imagePath = m.hexdigest() + '.png'
+            try:
+                imgkit.from_string(footer, dumpDir + '/' + imagePath, options = imgkitOptions)
+                writeLog('[INFO] Created footer %s' % imagePath)
+                imagesList.append(dumpDir + '/' + imagePath)
+            except:
+                writeLog('[WARNING] Creation of footer failed')
+
+        resultImage = dumpDir + '/' + 'new.png'
+        if len(imagesList) > 0:
+            images = list(map(Image.open, imagesList))
+            combo = appendImages(images)
+            combo.save(resultImage)
+            # Clean up temporary images
+            for i in imagesList:
+            os.remove(i)
+            return(resultImage)
+        else:
+            return(False)
--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 # EMailkasten
 
 A self-hosted mail archive based on django and React.js
+
+pre rendering based on code from xme's emlrender tool
