7fa7586f64d8b613ba426066e8ab899e030bb79f
david <18516197-Dacid99@users.noreply.gitlab.com>
2024-12-09 23:32:46 +0100

    tests: improved using pytest-mock

--- test/test_mailParsing.py
+++ test/test_mailParsing.py
@@ -1,3 +1,21 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# Emailkasten - a open-source self-hostable email archiving server
+# Copyright (C) 2024  David & Philipp Aderbauer
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import pytest
 from unittest.mock import patch
 from email.message import Message
@@ -5,8 +23,12 @@ import Emailkasten.mailParsing
 import Emailkasten.constants
 import datetime
 
+@pytest.fixture
+def mock_logger(mocker):
+    return mocker.patch('Emailkasten.mailParsing.logger')
+
 @pytest.fixture(scope='module')
-def test_good_mailMessage():
+def mock_good_mailMessage():
     testMessage = Message()
     testMessage.add_header("Message-ID", 'abcdefgäöüß§')
     testMessage.add_header("Subject", 'This a test SUBJEcT line äöüß§')
@@ -18,18 +40,18 @@ def test_good_mailMessage():
     return testMessage
 
 @pytest.fixture(scope='module')
-def test_special_mailMessage():
+def mock_special_mailMessage():
     testMessage = Message()
     testMessage.add_header("Subject", 'This a test SUBJEcT line äöüß§ \t ')
     return testMessage
 
 @pytest.fixture(scope='module')
-def test_bad_mailMessage():
+def mock_bad_mailMessage():
     testMessage = Message()
     return testMessage
 
 @pytest.fixture(scope='module')
-def test_no_mailMessage():
+def mock_no_mailMessage():
     testMessage = None
     return testMessage
 
@@ -40,7 +62,6 @@ def test_no_mailMessage():
         ('test äöüß§', "test äöüß§")
     ]
 )
-@patch('Emailkasten.mailParsing.logger')
 def test__decodeHeader(mock_logger, testHeader, expectedResult):
     decodedHeader = Emailkasten.mailParsing._decodeHeader(testHeader)
     assert decodedHeader == expectedResult
@@ -53,53 +74,52 @@ def test__decodeHeader(mock_logger, testHeader, expectedResult):
             (["Test Persön <testtestdomain.tld>"], [("Test Persön", "testtestdomain.tld")], True),
             (["test@testdomain.tld"], [("", "test@testdomain.tld")], False)
         ])
-@patch('Emailkasten.mailParsing.logger')
 def test__separateRFC2822MailAddressFormat(mock_logger, testMailers, expectedResult, warningCalled):
     separatedMailers = Emailkasten.mailParsing._separateRFC2822MailAddressFormat(testMailers)
     assert separatedMailers == expectedResult
 
-@patch('Emailkasten.mailParsing.logger')
-def test__parseMessageID_success(mock_logger, test_good_mailMessage):
-    messageID = Emailkasten.mailParsing._parseMessageID(test_good_mailMessage)
+
+def test__parseMessageID_success(mock_logger, mock_good_mailMessage):
+    messageID = Emailkasten.mailParsing._parseMessageID(mock_good_mailMessage)
     assert messageID == 'abcdefgäöüß§'
     mock_logger.debug.assert_called()
     mock_logger.warning.assert_not_called()
     mock_logger.error.assert_not_called()
 
-@patch('Emailkasten.mailParsing.logger')
-def test__parseMessageID_emptyMessage(mock_logger, test_bad_mailMessage):
-    messageID = Emailkasten.mailParsing._parseMessageID(test_bad_mailMessage)
-    assert messageID == str(hash(test_bad_mailMessage))
+
+def test__parseMessageID_emptyMessage(mock_logger, mock_bad_mailMessage):
+    messageID = Emailkasten.mailParsing._parseMessageID(mock_bad_mailMessage)
+    assert messageID == str(hash(mock_bad_mailMessage))
     mock_logger.debug.assert_called()
     mock_logger.warning.assert_called()
     mock_logger.error.assert_not_called()
 
-@pytest.mark.xfail
-@patch('Emailkasten.mailParsing.logger')
-def test__parseMessageID_noMessage(mock_logger, test_no_mailMessage):
-    messageID = Emailkasten.mailParsing._parseMessageID(test_no_mailMessage)
+
+def test__parseMessageID_noMessage(mock_logger, mock_no_mailMessage):
+    with pytest.raises(AttributeError):
+        messageID = Emailkasten.mailParsing._parseMessageID(mock_no_mailMessage)
 
 
-@patch('Emailkasten.mailParsing.logger')
-def test__parseDate_success(mock_logger, test_good_mailMessage):
-    date = Emailkasten.mailParsing._parseDate(test_good_mailMessage)
+
+def test__parseDate_success(mock_logger, mock_good_mailMessage):
+    date = Emailkasten.mailParsing._parseDate(mock_good_mailMessage)
     assert date == datetime.datetime(2001, 11, 9, 1, 8, 47)
     mock_logger.debug.assert_called()
     mock_logger.warning.assert_not_called()
     mock_logger.error.assert_not_called()
 
-@patch('Emailkasten.mailParsing.logger')
-def test__parseDate_emptyMessage(mock_logger, test_bad_mailMessage):
-    date = Emailkasten.mailParsing._parseDate(test_bad_mailMessage)
+
+def test__parseDate_emptyMessage(mock_logger, mock_bad_mailMessage):
+    date = Emailkasten.mailParsing._parseDate(mock_bad_mailMessage)
     assert date == datetime.datetime(1971, 1, 1, 0, 0, 0)
     mock_logger.debug.assert_called()
     mock_logger.warning.assert_called()
     mock_logger.error.assert_not_called()
 
-@pytest.mark.xfail
-@patch('Emailkasten.mailParsing.logger')
-def test__parseDate_noMessage(mock_logger, test_no_mailMessage):
-    date = Emailkasten.mailParsing._parseDate(test_no_mailMessage)
+
+def test__parseDate_noMessage(mock_logger, mock_no_mailMessage):
+    with pytest.raises(AttributeError):
+        date = Emailkasten.mailParsing._parseDate(mock_no_mailMessage)
 
 
 @pytest.mark.parametrize(
@@ -108,67 +128,67 @@ def test__parseDate_noMessage(mock_logger, test_no_mailMessage):
 )
 @patch("Emailkasten.mailParsing.ParsingConfiguration")
 @patch("Emailkasten.mailParsing.logger")
-def test__parseSubject_success(mock_logger, mock_parsingConfiguration, stripTexts, expectedResult, test_special_mailMessage):
+def test__parseSubject_success(mock_logger, mock_parsingConfiguration, stripTexts, expectedResult, mock_special_mailMessage):
     mock_parsingConfiguration.STRIP_TEXTS = stripTexts
-    subject = Emailkasten.mailParsing._parseSubject(test_special_mailMessage)
+    subject = Emailkasten.mailParsing._parseSubject(mock_special_mailMessage)
     assert subject == expectedResult
     mock_logger.debug.assert_called()
     mock_logger.warning.assert_not_called()
     mock_logger.error.assert_not_called()
 
-@patch('Emailkasten.mailParsing.logger')
-def test__parseSubject_emptyMessage(mock_logger, test_bad_mailMessage):
-    subject = Emailkasten.mailParsing._parseSubject(test_bad_mailMessage)
+
+def test__parseSubject_emptyMessage(mock_logger, mock_bad_mailMessage):
+    subject = Emailkasten.mailParsing._parseSubject(mock_bad_mailMessage)
     assert subject == ''
     mock_logger.debug.assert_called()
     mock_logger.warning.assert_called()
     mock_logger.error.assert_not_called()
 
-@pytest.mark.xfail
-@patch('Emailkasten.mailParsing.logger')
-def test__parseSubject_noMessage(mock_logger, test_no_mailMessage):
-    subject = Emailkasten.mailParsing._parseSubject(test_no_mailMessage)
+
+def test__parseSubject_noMessage(mock_logger, mock_no_mailMessage):
+    with pytest.raises(AttributeError):
+        subject = Emailkasten.mailParsing._parseSubject(mock_no_mailMessage)
 
 
-@patch('Emailkasten.mailParsing.logger')
-def test__parseHeader_success(mock_logger, test_good_mailMessage):
-    header = Emailkasten.mailParsing._parseHeader(test_good_mailMessage, "test")
+
+def test__parseHeader_success(mock_logger, mock_good_mailMessage):
+    header = Emailkasten.mailParsing._parseHeader(mock_good_mailMessage, "test")
     assert header == "test äöüß§"
     mock_logger.debug.assert_called()
     mock_logger.warning.assert_not_called()
     mock_logger.error.assert_not_called()
 
-@patch('Emailkasten.mailParsing.logger')
-def test__parseHeader_emptyMessage(mock_logger, test_bad_mailMessage):
-    header = Emailkasten.mailParsing._parseHeader(test_bad_mailMessage, "test")
+
+def test__parseHeader_emptyMessage(mock_logger, mock_bad_mailMessage):
+    header = Emailkasten.mailParsing._parseHeader(mock_bad_mailMessage, "test")
     assert header == ''
     mock_logger.debug.assert_called()
     mock_logger.warning.assert_not_called()
     mock_logger.error.assert_not_called()
 
-@pytest.mark.xfail
-@patch('Emailkasten.mailParsing.logger')
-def test__parseHeader_noMessage(mock_logger, test_no_mailMessage):
-    header = Emailkasten.mailParsing._parseHeader(test_no_mailMessage, "test")
+
+def test__parseHeader_noMessage(mock_logger, mock_no_mailMessage):
+    with pytest.raises(AttributeError):
+        header = Emailkasten.mailParsing._parseHeader(mock_no_mailMessage, "test")
 
 
-@patch('Emailkasten.mailParsing.logger')
-def test__parseMultipleHeader_success(mock_logger, test_good_mailMessage):
-    header = Emailkasten.mailParsing._parseMultipleHeader(test_good_mailMessage, "multi")
+
+def test__parseMultipleHeader_success(mock_logger, mock_good_mailMessage):
+    header = Emailkasten.mailParsing._parseMultipleHeader(mock_good_mailMessage, "multi")
     assert header == "test\näöüß§\t\n123456"
     mock_logger.debug.assert_called()
     mock_logger.warning.assert_not_called()
     mock_logger.error.assert_not_called()
 
-@patch('Emailkasten.mailParsing.logger')
-def test__parseMultipleHeader_emptyMessage(mock_logger, test_bad_mailMessage):
-    header = Emailkasten.mailParsing._parseMultipleHeader(test_bad_mailMessage, "multi")
+
+def test__parseMultipleHeader_emptyMessage(mock_logger, mock_bad_mailMessage):
+    header = Emailkasten.mailParsing._parseMultipleHeader(mock_bad_mailMessage, "multi")
     assert header == ''
     mock_logger.debug.assert_called()
     mock_logger.warning.assert_not_called()
     mock_logger.error.assert_not_called()
 
-@pytest.mark.xfail
-@patch('Emailkasten.mailParsing.logger')
-def test__parseMultipleHeader_noMessage(mock_logger, test_no_mailMessage):
-    header = Emailkasten.mailParsing._parseMultipleHeader(test_no_mailMessage, "multi")
+
+def test__parseMultipleHeader_noMessage(mock_logger, mock_no_mailMessage):
+    with pytest.raises(AttributeError):
+        header = Emailkasten.mailParsing._parseMultipleHeader(mock_no_mailMessage, "multi")
