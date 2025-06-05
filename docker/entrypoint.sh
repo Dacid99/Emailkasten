962e9c6a6237807e262c95c131db0da84f6f387a
david <18516197-Dacid99@users.noreply.gitlab.com>
2025-06-05 10:31:06 +0200

    feat (docker): wsgi deployment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 poetry run python manage.py migrate
 poetry run python manage.py createsuperuser --username=admin --email=  --noinput
-poetry run python manage.py runserver 0.0.0.0:8000
+poetry run gunicorn Emailkasten.wsgi --bind 0.0.0.0:8000
